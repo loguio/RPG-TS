391d6b2e694a2b808912e168f88578fa8590a152
Marius BOURSE <marius.bourse@ynov.com>
2022-05-03 00:35:41 +0200

    choix du perso

--- GameManager.ts
+++ GameManager.ts
@@ -1,9 +1,15 @@
+import {Menu} from "./menu.ts";
+
 class GameManager {
     startGame() {
-
+        new Menu().chooseCharacter();
     }
 
     fight() {
         
     }
+    constructor() {
+        this.startGame();
+    }
 }
+let gameManager = new GameManager();
--- menu.ts
+++ menu.ts
@@ -1,4 +1,4 @@
-class Menu {
+export class Menu {
     showCharacter() {
 
     }
@@ -18,4 +18,16 @@ class Menu {
     useItems() {
         
     }
+    choice: string="";
+    chooseCharacter() {
+        while (this.choice == "") {
+            console.log("Choose your character by typing the number of the character you want to play");
+            console.log("1. Guerrier \n2. Mage \n3. Paladin \n4. Barbare \n5. Prêtre \n6. Voleur");
+            let temp = prompt("Votre choix :");
+            if (temp != null && temp != "" && temp >= "1" && temp <= "6") {
+                this.choice = temp;
+            }
+        }
+    }
+    constructor(){}
 }
