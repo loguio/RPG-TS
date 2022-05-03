828f0e52323c8938cdc0565d79fcfa61264e1f58
Marius BOURSE <marius.bourse@ynov.com>
2022-05-03 08:35:24 +0200

    choix de 3 perso

--- menu.ts
+++ menu.ts
@@ -18,14 +18,18 @@ export class Menu {
     useItems() {
         
     }
-    choice: string="";
+    choice: string[]=[];
     chooseCharacter() {
-        while (this.choice == "") {
+        while (this.choice.length < 3) {
             console.log("Choose your character by typing the number of the character you want to play");
             console.log("1. Guerrier \n2. Mage \n3. Paladin \n4. Barbare \n5. Prêtre \n6. Voleur");
             let temp = prompt("Votre choix :");
             if (temp != null && temp != "" && temp >= "1" && temp <= "6") {
-                this.choice = temp;
+                if (this.choice[0] == temp || this.choice[1] == temp) {
+                    console.log("vous ne pouvez pas choisir deux fois le même personnage");
+                }else{
+                    this.choice.push(temp);
+                }
             }
         }
     }
