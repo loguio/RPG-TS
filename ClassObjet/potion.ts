e4a962d2b92c43259ee835cda0ff9a0649678088
JulesOutin <jules.outin@ynov.com>
2022-05-05 10:14:34 +0200

    Potion

--- ClassObjet/potion.ts
+++ ClassObjet/potion.ts
@@ -1,9 +1,11 @@
 
 class Potion  {
+    heal : number = 50;
     /**
      * Cette function permet de soigner un personnage en lui rendant 50% de pv.
      */
     potion(){
-    
-    }
+        if (this.life > 0){this.life = this.life + this.heal} 
+        else if (this.life > 50) {}
+    } 
 }
