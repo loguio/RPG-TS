4204ca2072c78ed297519958e47f141f57aa1d5a
JulesOutin <jules.outin@ynov.com>
2022-05-09 10:32:31 +0200

    Update

--- ClassObjet/DemiEtoile.ts
+++ ClassObjet/DemiEtoile.ts
@@ -1,6 +1,7 @@
 import Perso from "../caracter/Personnage.ts"
 
 export default class DemiEtoile extends Perso{
+    name : string = "DemiEtoile";
     demiEtoile(){
         if (this.alive == false) {this.alive = this.alive}
         else if (this.life > 0 ) {this.life = 100}
--- ClassObjet/potion.ts
+++ ClassObjet/potion.ts
@@ -3,6 +3,7 @@ import Perso from "../caracter/Personnage.ts"
 
 export default class Potion extends Perso {
     heal : number = 50;
+    name : string = "Potion";
     /**
      * Cette function permet de soigner un personnage en lui rendant 50% de pv.
      */
