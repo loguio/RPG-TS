0089235f58a13d6a05b52cbe1a194b91c762f958
Jules <jules.outin@ynov.com>
2022-05-17 14:16:26 +0200

    Changement des noms

--- character/Pretre.ts
+++ character/Pretre.ts
@@ -6,4 +6,11 @@ export default class Pretre extends Perso {
     name = "Pretre"
     mana : number = 40;
     manaMax : number = 40
+
+    Pretre(ally: Perso) {
+        if (this.mana > 0) {
+            ally.life += this.life/4;
+            this.mana -= 10;
+        }
+    }
 }
